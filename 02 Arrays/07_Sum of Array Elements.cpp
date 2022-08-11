eef09ffa2d1de5340b564f6ff8f2c8929a5367d2
Harsh Srivastava <88231697+harsh-srivastv@users.noreply.github.com>
2022-08-11 12:17:53 +0530

    Add files via upload

---
+++ 02 Arrays/07_Sum of Array Elements.cpp
@@ -0,0 +1,48 @@
+// https://practice.geeksforgeeks.org/problems/sum-of-array-elements2502/1
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+//Complete this function
+
+int sumElement(int arr[],int n)
+{
+    //Your code here
+    int sum = 0;
+    for(int i=0;i<n;i++)
+    {
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    cin>>arr[i];
+	    
+	    cout<<sumElement(arr,n)<<endl;
+	    
+	}
+	return 0;
+}
+// } Driver Code Ends
