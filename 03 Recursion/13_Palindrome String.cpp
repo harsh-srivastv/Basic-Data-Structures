7581a2b26ebfe1b58892bb8265368cdc5b713d4a
Harsh Srivastava <88231697+harsh-srivastv@users.noreply.github.com>
2022-08-15 11:44:41 +0530

    Add files via upload

---
+++ 03 Recursion/13_Palindrome String.cpp
@@ -0,0 +1,55 @@
+// https://practice.geeksforgeeks.org/problems/palindrome-string0817/1
+
+//{ Driver Code Starts
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	
+	int isPalindrome(string S)
+	{
+	    // Your code goes here
+	    int i=0;
+	    int j=S.length()-1;
+	    
+	    while(i<=j){
+	        if(S[i]!=S[j]){
+	            return 0;
+	        }
+	        i++;
+	        j--;
+	    }
+	    return 1;
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   	    Solution ob;
+
+   		cout << ob.isPalindrome(s) << "\n";
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
