8fab78556350f8e2c25f153dce14bf5fc9443755
Harsh Srivastava <88231697+harsh-srivastv@users.noreply.github.com>
2022-10-18 14:26:59 +0530

    Add files via upload

---
+++ 09 Circular Linked List/01 Check If Circular Linked List.cpp
@@ -0,0 +1,90 @@
+// https://practice.geeksforgeeks.org/problems/circular-linked-list/1
+
+//{ Driver Code Starts
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+
+/* Function to get the middle of the linked list*/
+bool isCircular(struct Node *head);
+
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l,k;
+
+    cin>>T;
+
+    while(T--){
+
+        cin>>n>>k;
+	    Node *head=NULL, *tail = NULL;
+	    int x;
+	    cin >> x;
+	    head = new Node(x);
+	    tail = head;
+	    for(int i=0;i<n-1;i++)
+	    {
+	        cin>>x;
+	        tail -> next = new Node(x);
+	        tail = tail -> next;
+	    }
+        if (k==1 && n >= 1)
+              tail->next = head;
+
+
+    printf("%d\n", isCircular(head));
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
+
+
+/* Link list Node 
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+}; 
+*/
+
+/* Should return true if linked list is circular, else false */
+bool isCircular(Node *head)
+{
+   if(head == NULL) return true;
+   
+   if(head == head->next) return true;
+   
+   Node* curr = head;
+   while(curr){
+       curr = curr->next;
+       if(curr == head){
+           return true;
+       }
+   }
+   return false;
+}
